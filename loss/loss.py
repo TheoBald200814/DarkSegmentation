faed14d4cdd84028b68c63034a5f50318672d6f2
TheoBald200814 <274429919@qq.com>
2023-12-21 21:42:28 +0800

    update loss function

--- loss/loss.py
+++ loss/loss.py
@@ -1,6 +1,7 @@
 from loss import custom_l2_loss
 from loss import perceptual_loss
 from loss import ssim_loss as sl
+from torch import nn
 
 
 def loss_function(prediction, label, vgg16_layer_number, lambda_1, lambda_2):
@@ -17,6 +18,17 @@ def loss_function(prediction, label, vgg16_layer_number, lambda_1, lambda_2):
 
     loss_total = per_loss + lambda_1 * ssim_loss + lambda_2 * l2_loss
     return loss_total
+
+class MyLoss(nn.Module):
+    def __init__(self, vgg16_layer_number, lambda_1, lambda_2):
+        super(MyLoss, self).__init__()
+        self.vgg16_layer_number = vgg16_layer_number
+        self.lambda_1 = lambda_1
+        self.lambda_2 = lambda_2
+
+    def forward(self, prediction, label):
+        return loss_function(prediction, label, vgg16_layer_number=self.vgg16_layer_number, lambda_1=self.lambda_1, lambda_2=self.lambda_2)
+
 
 
 
